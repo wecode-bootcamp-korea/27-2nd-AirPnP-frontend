a9e26332ffe468f81c60d2cbb88e3db916168a8d
sanha <kgpaper@naver.com>
2021-12-13 20:19:09 +0900

    Add: Nav바 구현 - nav 레이아웃 작성 - 햄버거 메뉴 및 검색 패널 조건부 렌더링 - react-datepicker 라이브러리를 통한 데이피커 구현 - 데이피커 css 적용 - 리뷰 반영 / 리팩토링 - 상수데이터 사용

---
+++ src/components/Nav/ExtendedSearchBar/ExtendedSearchBar.js
@@ -0,0 +1,132 @@
+import React, { useState, useRef } from 'react';
+import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import { BiSearch } from 'react-icons/bi';
+import { ko } from 'date-fns/esm/locale';
+import './datepicker.scss';
+import CustomInput from './CustomInput/CustomInput';
+
+const ExtendedSearchBar = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [dateInput, setDateInput] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+  const talentInputRef = useRef();
+
+  const adjustDate = (type, date) => {
+    setDateInput(prevDate => ({ ...prevDate, [type]: date }));
+  };
+
+  const changeSearchInput = e => {
+    setSearchInput(() => e.target.value);
+  };
+
+  return (
+    <SearchPannel>
+      <PannelButton onClick={() => talentInputRef.current.focus()}>
+        <ButtonName>재능</ButtonName>
+        <TalentInput
+          value={searchInput}
+          type="text"
+          placeholder="어떤 재능을 찾으시나요?"
+          onChange={changeSearchInput}
+          ref={talentInputRef}
+        />
+      </PannelButton>
+      <DatePicker
+        selected={dateInput.startDate}
+        startDate={dateInput.startDate}
+        endDate={dateInput.endDate}
+        minDate={new Date()}
+        dateFormat={DATE_FORMAT}
+        dateFormatCalendar={DATE_FORMAT_CALENDAR}
+        onChange={date => adjustDate(START_DATE_TYPE, date)}
+        locale={ko}
+        customInput={<CustomInput inputType="startDate" />}
+      />
+      <DatePicker
+        selected={dateInput.endDate}
+        startDate={dateInput.startDate}
+        endDate={dateInput.endDate}
+        minDate={dateInput.startDate}
+        dateFormat={DATE_FORMAT}
+        dateFormatCalendar={DATE_FORMAT_CALENDAR}
+        onChange={date => adjustDate(END_DATE_TYPE, date)}
+        locale={ko}
+        customInput={<CustomInput inputType="endDate" />}
+      />
+      <SearchClick>
+        <BiSearch />
+      </SearchClick>
+    </SearchPannel>
+  );
+};
+
+const START_DATE_TYPE = 'startDate';
+const END_DATE_TYPE = 'endDate';
+const DATE_FORMAT = 'yyyy년 MM월 dd일';
+const DATE_FORMAT_CALENDAR = 'yyyy년 MM월';
+
+const SearchPannel = styled.form`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  top: 90px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: white;
+  border: 1px solid ${({ theme }) => theme.lightGray};
+  border-radius: 45px;
+`;
+
+const PannelButton = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 18px 30px;
+  margin-right: 2px;
+  border-radius: 50px;
+  border-color: transparent;
+  background-color: white;
+  transition: 0.4s ease;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 3px 0 ${({ theme }) => theme.middleGray};
+  }
+`;
+
+const SearchClick = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 10px;
+  padding: 16px;
+  border-radius: 50px;
+  border-color: transparent;
+  width: auto;
+  background-color: ${({ theme }) => theme.highlight};
+  font-size: 24px;
+  color: ${({ theme }) => theme.white};
+  cursor: pointer;
+`;
+
+const ButtonName = styled.p`
+  margin: 0 0 4px 2px;
+  font-size: 12px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.black};
+`;
+
+const TalentInput = styled.input`
+  min-width: 200px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default ExtendedSearchBar;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,194 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { BiSearch } from 'react-icons/bi';
+import { IoMenuOutline } from 'react-icons/io5';
+import ExtendedSearchBar from './ExtendedSearchBar/ExtendedSearchBar';
 
-function Nav() {
-  return <div />;
-}
+const Nav = () => {
+  const [isMenuExtend, setIsMenuExtend] = useState(false);
+  const [isSearchExtend, setIsSearchExtend] = useState(false);
+
+  return (
+    <>
+      <NavLayout />
+      <NavStyle>
+        <LogoContainer>
+          <Logo>airpnp</Logo>
+        </LogoContainer>
+        <SearchContainer>
+          <SearchButton onClick={() => setIsSearchExtend(prev => !prev)}>
+            <SearchText>검색 시작하기</SearchText>
+            <SearchCircle>
+              <BiSearch />
+            </SearchCircle>
+          </SearchButton>
+        </SearchContainer>
+        {isSearchExtend && <ExtendedSearchBar />}
+        <MenuContainer>
+          <ToHost>호스트 등록</ToHost>
+          <ExtentionMenu onClick={() => setIsMenuExtend(prev => !prev)}>
+            <IoMenuOutline />
+            <ProfileImg src="/images/peopleicon.jpeg" alt="profile" />
+            {isMenuExtend && (
+              <AppearedMenu>
+                <MenuList>
+                  <LoginListElement>로그인</LoginListElement>
+                  <ListElement>호스트 등록</ListElement>
+                  <ListElement>마이페이지</ListElement>
+                </MenuList>
+              </AppearedMenu>
+            )}
+          </ExtentionMenu>
+        </MenuContainer>
+      </NavStyle>
+    </>
+  );
+};
+
+const NavLayout = styled.div`
+  height: 80px;
+`;
+
+const NavStyle = styled.nav`
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  height: 80px;
+  padding: 18px 80px 0 80px;
+  background-color: white;
+  box-shadow: 0 0 5px 0 gray;
+  z-index: 999;
+`;
+
+const LogoContainer = styled.div`
+  width: 37%;
+`;
+
+const Logo = styled.div`
+  width: 100%;
+`;
+
+const SearchContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 26%;
+`;
+
+const SearchButton = styled.button`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 280px;
+  height: 42px;
+  background-color: white;
+  border: 1px solid transparent;
+  border-radius: 40px;
+  box-shadow: 0 0 3px 0 ${({ theme }) => theme.lightGray};
+  transition: 0.4s ease;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 3px 0 ${({ theme }) => theme.middleGray};
+  }
+`;
+
+const SearchText = styled.div`
+  margin-left: 10px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.middleGray};
+`;
+
+const SearchCircle = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.highlight};
+  font-size: 16px;
+  color: ${({ theme }) => theme.white};
+`;
+
+const ToHost = styled.div`
+  padding-right: 20px;
+  border: none;
+  background-color: transparent;
+  font-size: 12px;
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+const ExtentionMenu = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  margin: 0 10px;
+  padding: 5px 5px 5px 12px;
+  border-radius: 22px;
+  border: 1px solid ${({ theme }) => theme.lightGray};
+  background-color: white;
+  font-size: 20px;
+  cursor: pointer;
+`;
+const ProfileImg = styled.img`
+  margin-left: 12px;
+  width: 30px;
+  height: 30px;
+  object-fit: cover;
+  border-radius: 16px;
+`;
+
+const AppearedMenu = styled.div`
+  position: absolute;
+  top: 50px;
+  right: 0;
+  text-align: left;
+  padding: 12px 0;
+  width: 30vh;
+  background-color: white;
+  box-shadow: 0 0 3px 0 ${({ theme }) => theme.middleGray};
+  border-radius: 16px;
+`;
+
+const MenuList = styled.ul`
+  width: 100%;
+`;
+
+const ListElement = styled.li`
+  padding: 16px 16px;
+  font-size: 14px;
+  color: #222222;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #f7f7f7;
+  }
+`;
+
+const LoginListElement = styled.li`
+  padding: 16px 16px;
+  border-bottom: 1px solid ${({ theme }) => theme.lightGray};
+  font-size: 14px;
+  color: #222222;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #f7f7f7;
+  }
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 37%;
+`;
 
 export default Nav;
