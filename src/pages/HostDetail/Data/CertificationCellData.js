1128c7dcf040cbfdaefdabf299fa38fce8bf818e
niboo <moonevening8@gmail.com>
2021-12-14 14:43:51 +0900

    ADD: 상세페이지 기능 구현 중

---
+++ src/pages/HostDetail/Data/CertificationCellData.js
@@ -0,0 +1,21 @@
+import { AiFillStar } from 'react-icons/ai';
+import { AiOutlineSafetyCertificate } from 'react-icons/ai';
+import { AiFillHeart } from 'react-icons/ai';
+
+export const CertificationCellData = [
+  {
+    id: 1,
+    icon: <AiFillStar />,
+    letter: 'Activity',
+  },
+  {
+    id: 2,
+    icon: <AiOutlineSafetyCertificate />,
+    letter: '본인 인증 완료',
+  },
+  {
+    id: 3,
+    icon: <AiFillHeart />,
+    letter: '슈퍼맨',
+  },
+];
