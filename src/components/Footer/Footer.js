1e1e1fe9d4d05695952e739d99b241a641e174da
niboo <moonevening8@gmail.com>
2021-12-21 18:33:53 +0900

    ADD: footer완성

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,11 +10,13 @@ function Footer() {
     <FooterWrapper>
       <FooterTop>
         <FooterTopBox>
-          <BoxTitle>에어피앤피</BoxTitle>
-          <BoxList>Front-end</BoxList>
-          <BoxList>길도연 · 김혜리 · 이나은</BoxList>
-          <BoxList>Back-end</BoxList>
-          <BoxList>김은혜 · 염기욱 · 이주명</BoxList>
+          <FooterAirPnP>에어피앤피</FooterAirPnP>
+          <FooterIntro>
+            <BoxTitle>Front-end</BoxTitle>
+            <BoxName>길도연 · 김혜리 · 이나은</BoxName>
+            <BoxTitle>Back-end</BoxTitle>
+            <BoxName>김은혜 · 염기욱 · 이주명</BoxName>
+          </FooterIntro>
         </FooterTopBox>
       </FooterTop>
       <FooterBottom>
@@ -37,12 +39,10 @@ const FooterWrapper = styled.footer`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: fixed;
   bottom: 0;
   width: 100%;
-  height: 250px;
+  height: 150px;
   background-color: ${props => props.theme.lightGray};
-  border: ${props => props.theme.borderMiddleGray};
 `;
 const FooterTop = styled.div`
   display: flex;
@@ -53,32 +53,42 @@ const FooterTop = styled.div`
 const FooterTopBox = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  width: 20%;
-  height: 85%;
-  margin: 20px 20px 20px 60px;
+  justify-content: flex-start;
+  width: 100%;
+  height: 80%;
+  margin-left: 15px;
+`;
+const FooterAirPnP = styled.span`
+  margin-top: 20px;
+  color: ${props => props.theme.darkGray};
+  font-size: 13px;
+`;
+const FooterIntro = styled.div`
+  display: flex;
 `;
 const BoxTitle = styled.span`
-  margin-bottom: 20px;
+  width: 100px;
+  margin-top: 25px;
   color: ${props => props.theme.darkGray};
   font-size: 15px;
 `;
-const BoxList = styled.span`
+const BoxName = styled.span`
+  width: 200px;
+  margin-top: 25px;
   color: ${props => props.theme.darkGray};
-  font-size: 13px;
+  font-size: 15px;
 `;
 const FooterBottom = styled.div`
   display: flex;
   justify-content: center;
   width: 90%;
   height: 20%;
-  margin-top: 20px;
   border-top: ${props => props.theme.borderMiddleGray};
   text-align: center;
 `;
 const FooterBottomLetter = styled.span`
   width: 90%;
-  margin: 20px 15px 0 0;
+  margin: 10px 15px 0 0;
   color: ${props => props.theme.darkGray};
   font-size: 13px;
   letter-spacing: 0.5px;
