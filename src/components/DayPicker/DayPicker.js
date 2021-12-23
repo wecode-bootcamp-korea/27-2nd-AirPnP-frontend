9dc90edf9190f2b20f60b7d299e0026acbac8c7f
sanha <kgpaper@naver.com>
2021-12-23 17:08:08 +0900

    Add: 예약된 날짜 disabled - 예약된 날짜는 선택하지 못하게 datepicker를 수정했습니다.

--- src/components/DayPicker/DayPicker.js
+++ src/components/DayPicker/DayPicker.js
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import CustomInput from './CustomInput/CustomInput';
 import { ko } from 'date-fns/esm/locale';
 import './datepicker.scss';
+import { subDays } from 'date-fns';
 
-const DayPicker = ({ type, dateInput, adjustDate }) => {
+const DayPicker = ({ type, dateInput, adjustDate, bookingDate }) => {
+  const [bookedData, setBookedData] = useState([]);
+
+  useEffect(() => {
+    if (bookingDate) {
+      const handdledData = bookingDate.map(({ start_date, end_date }) => ({
+        start: subDays(new Date(start_date), 1),
+        end: new Date(end_date),
+      }));
+
+      setBookedData(handdledData);
+    }
+  }, [bookingDate]);
+
   return (
     <DatePicker
       selected={type === 'start' ? dateInput.startDate : dateInput.endDate}
       startDate={dateInput.startDate}
       endDate={dateInput.endDate}
-      minDate={new Date()}
+      minDate={type === 'start' ? new Date() : dateInput.startDate}
       dateFormat={DATE_FORMAT}
       dateFormatCalendar={DATE_FORMAT_CALENDAR}
       onChange={date =>
         adjustDate(type === 'start' ? START_DATE_TYPE : END_DATE_TYPE, date)
       }
+      excludeDateIntervals={bookedData}
       locale={ko}
       customInput={<CustomInput inputType={type} />}
     />
